ec188f201d442bd1ebf0d68ecef9230ba431e4be
Allan Dayrell <allan.dayrell@gmail.com>
2025-08-20 16:52:32 -0300

    feat: Leitura de argumentos via sys.argv

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import sys
+
 def main():
     """Função principal qie orquestra o script."""
     print("Iniciando o conversor...")
+
+    argumentos = sys.argv
+    print(f"Argumentos recebidos: {argumentos}")
+
 
 if __name__ == "__main__":
     main()
